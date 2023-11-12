545ed1aba1a358a72bc008b26202d3b21b053063
BrandonWu500 <brwu17@gmail.com>
2023-11-11 20:15:36 -0500

    fix: invisible popover from showing

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,2 @@
 // To prevent brief flicker on screen before alpine js has loaded to hide mobile menu
 document.querySelector('#mobile-menu').classList.remove('hidden');
-document.querySelector('#global-cart-popover').classList.remove('hidden');
