ae36147d1a07f32453b8f75d4a13406ab846c29b
BrandonWu500 <brwu17@gmail.com>
2023-11-12 16:54:02 -0500

    feat: create mobile collection page

---
+++ assets/collection.js
@@ -0,0 +1,39 @@
+const handleFilterSectionButtonState = (button) => {
+  if (button.classList.contains('filter-section-button-expanded')) {
+    const expandIcon = button.querySelector('.expand-icon');
+    expandIcon.classList.add('hidden');
+    const collapseIcon = button.querySelector('.collapse-icon');
+    collapseIcon.classList.remove('hidden');
+
+    const filterSectionMobile = button.closest('.filter-section-mobile');
+    filterSectionMobile
+      .querySelector('.filter-section-mobile-values')
+      .classList.remove('hidden');
+
+    button.classList.toggle('filter-section-button-expanded');
+  } else {
+    const expandIcon = button.querySelector('.expand-icon');
+    expandIcon.classList.remove('hidden');
+    const collapseIcon = button.querySelector('.collapse-icon');
+    collapseIcon.classList.add('hidden');
+
+    const filterSectionMobile = button.closest('.filter-section-mobile');
+    filterSectionMobile
+      .querySelector('.filter-section-mobile-values')
+      .classList.add('hidden');
+
+    button.classList.toggle('filter-section-button-expanded');
+  }
+};
+
+const mobileFilterSectionButtons = document.querySelectorAll(
+  '.filter-section-button'
+);
+
+mobileFilterSectionButtons.forEach((button) => {
+  handleFilterSectionButtonState(button);
+
+  button.addEventListener('click', () =>
+    handleFilterSectionButtonState(button)
+  );
+});
