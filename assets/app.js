710efd3224cd006b7bdb89f126470d79608d8f54
BrandonWu500 <brwu17@gmail.com>
2023-11-13 15:30:57 -0500

    chore: clean up comments

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,3 @@
-// To prevent brief flicker on screen before alpine js has loaded to hide mobile menu
+// To prevent brief flicker on screen before alpine js has loaded to hide stuff
 document.querySelector('#mobile-menu').classList.remove('hidden');
 document.querySelector('#mobile-filters').classList.remove('hidden');
--- assets/collection.js
+++ assets/collection.js
@@ -15,28 +15,6 @@ const expandOrCollapseFilter = (button) => {
   const svgArrow = filterSection.querySelector('svg');
   svgArrow.classList.toggle('rotate-0');
   svgArrow.classList.toggle('-rotate-180');
-
-  // if (button.classList.contains('filter-section-button-expanded')) {
-  //   const expandIcon = button.querySelector('.expand-icon');
-  //   expandIcon.classList.add('hidden');
-  //   const collapseIcon = button.querySelector('.collapse-icon');
-  //   collapseIcon.classList.remove('hidden');
-
-  //   const filterSectionMobile = button.closest('.filter-section-mobile');
-  //   filterSectionMobile
-  //     .querySelector('.filter-section-mobile-values')
-  //     .classList.remove('hidden');
-  // } else {
-  //   const expandIcon = button.querySelector('.expand-icon');
-  //   expandIcon.classList.remove('hidden');
-  //   const collapseIcon = button.querySelector('.collapse-icon');
-  //   collapseIcon.classList.add('hidden');
-
-  //   const filterSectionMobile = button.closest('.filter-section-mobile');
-  //   filterSectionMobile
-  //     .querySelector('.filter-section-mobile-values')
-  //     .classList.add('hidden');
-  // }
 };
 
 const collpaseAllFiltersExcept = (filterSection = null) => {
@@ -75,15 +53,6 @@ const desktopSortSelect = dekstopFilterAndSortForm.querySelector(
 desktopSortSelect.addEventListener('change', () => {
   dekstopFilterAndSortForm.submit();
 });
-
-// const filterCheckboxes = filterAndSortForm.querySelectorAll(
-//   'input[type="checkbox"]'
-// );
-// filterCheckboxes.forEach((checkbox) => {
-//   checkbox.addEventListener('change', () => {
-//     filterAndSortForm.submit();
-//   });
-// });
 
 const allFilterSectionValues = document.querySelectorAll(
   '.filter-section-values'
