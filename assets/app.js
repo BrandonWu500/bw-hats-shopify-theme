22f46a90c79fcac01ee3c349c698bd1a984bd958
BrandonWu500 <brwu17@gmail.com>
2023-11-12 22:17:57 -0500

    feat: add apply filters button
    and separate sort from filters

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,3 @@
 // To prevent brief flicker on screen before alpine js has loaded to hide mobile menu
 document.querySelector('#mobile-menu').classList.remove('hidden');
+document.querySelector('#mobile-filters').classList.remove('hidden');
--- assets/collection.js
+++ assets/collection.js
@@ -1,47 +1,56 @@
-const handleFilterSectionButtonState = (button) => {
-  if (button.classList.contains('filter-section-button-expanded')) {
-    const expandIcon = button.querySelector('.expand-icon');
-    expandIcon.classList.add('hidden');
-    const collapseIcon = button.querySelector('.collapse-icon');
-    collapseIcon.classList.remove('hidden');
+const expandOrCollapseFilter = (button) => {
+  const filterSection = button.closest('.filter-section');
+  const filterSectionValues = filterSection.querySelector(
+    '.filter-section-values'
+  );
+  filterSectionValues.classList.toggle('hidden');
 
-    const filterSectionMobile = button.closest('.filter-section-mobile');
-    filterSectionMobile
-      .querySelector('.filter-section-mobile-values')
-      .classList.remove('hidden');
+  const svgArrow = filterSection.querySelector('svg');
+  svgArrow.classList.toggle('rotate-0');
+  svgArrow.classList.toggle('-rotate-180');
 
-    button.classList.toggle('filter-section-button-expanded');
-  } else {
-    const expandIcon = button.querySelector('.expand-icon');
-    expandIcon.classList.remove('hidden');
-    const collapseIcon = button.querySelector('.collapse-icon');
-    collapseIcon.classList.add('hidden');
+  // if (button.classList.contains('filter-section-button-expanded')) {
+  //   const expandIcon = button.querySelector('.expand-icon');
+  //   expandIcon.classList.add('hidden');
+  //   const collapseIcon = button.querySelector('.collapse-icon');
+  //   collapseIcon.classList.remove('hidden');
 
-    const filterSectionMobile = button.closest('.filter-section-mobile');
-    filterSectionMobile
-      .querySelector('.filter-section-mobile-values')
-      .classList.add('hidden');
+  //   const filterSectionMobile = button.closest('.filter-section-mobile');
+  //   filterSectionMobile
+  //     .querySelector('.filter-section-mobile-values')
+  //     .classList.remove('hidden');
+  // } else {
+  //   const expandIcon = button.querySelector('.expand-icon');
+  //   expandIcon.classList.remove('hidden');
+  //   const collapseIcon = button.querySelector('.collapse-icon');
+  //   collapseIcon.classList.add('hidden');
 
-    button.classList.toggle('filter-section-button-expanded');
-  }
+  //   const filterSectionMobile = button.closest('.filter-section-mobile');
+  //   filterSectionMobile
+  //     .querySelector('.filter-section-mobile-values')
+  //     .classList.add('hidden');
+  // }
 };
 
-const mobileFilterSectionButtons = document.querySelectorAll(
-  '.filter-section-button'
+const expandCollapseFilterButtons = document.querySelectorAll(
+  '.expand-collapse-filter-button'
 );
 
-mobileFilterSectionButtons.forEach((button) => {
-  handleFilterSectionButtonState(button);
-
-  button.addEventListener('click', () =>
-    handleFilterSectionButtonState(button)
-  );
+expandCollapseFilterButtons.forEach((button) => {
+  button.addEventListener('click', () => expandOrCollapseFilter(button));
 });
 
-const mobileFilterForm = document.querySelector('#mobile-filter-form');
-if (mobileFilterForm) {
-  const sortSelect = document.querySelector('#mobile-filter-form select');
-  sortSelect.addEventListener('change', () => {
-    mobileFilterForm.submit();
-  });
-}
+const sortForm = document.querySelector('#sort-form');
+const sortSelect = document.querySelector('#sort-form select');
+sortSelect.addEventListener('change', () => {
+  sortForm.submit();
+});
+
+// const filterCheckboxes = sortForm.querySelectorAll(
+//   'input[type="checkbox"]'
+// );
+// filterCheckboxes.forEach((checkbox) => {
+//   checkbox.addEventListener('change', () => {
+//     sortForm.submit();
+//   });
+// });
