ed9dc7ba94556c45ca5f366d0e0e1c053fb8a56f
BrandonWu500 <brwu17@gmail.com>
2023-11-14 15:31:55 -0500

    feat: create blog page

--- assets/search.js
+++ assets/search.js
@@ -2,13 +2,27 @@ const searchPageCheckboxesFieldset = document.querySelector(
   '#search-page-checkboxes'
 );
 
-const searchPageCheckboxes =
-  searchPageCheckboxesFieldset.querySelectorAll('input');
+if (searchPageCheckboxesFieldset) {
+  const searchPageCheckboxes =
+    searchPageCheckboxesFieldset.querySelectorAll('input');
 
-const searchType = document.querySelector('#search-type');
+  const searchType = document.querySelector('#search-type');
 
-searchPageCheckboxes.forEach((checkbox) => {
-  checkbox.addEventListener('change', () => {
+  searchPageCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', () => {
+      let searchTypeValue = '';
+      searchPageCheckboxes.forEach((input) => {
+        if (input.checked) {
+          searchTypeValue = searchTypeValue.concat(input.name + ',');
+        }
+      });
+      searchType.value = searchTypeValue;
+    });
+  });
+
+  const searchBarForm = document.querySelector('#search-bar-form');
+  searchBarForm.addEventListener('submit', (event) => {
+    event.preventDefault();
     let searchTypeValue = '';
     searchPageCheckboxes.forEach((input) => {
       if (input.checked) {
@@ -16,18 +30,6 @@ searchPageCheckboxes.forEach((checkbox) => {
       }
     });
     searchType.value = searchTypeValue;
+    searchBarForm.submit();
   });
-});
-
-const searchBarForm = document.querySelector('#search-bar-form');
-searchBarForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  let searchTypeValue = '';
-  searchPageCheckboxes.forEach((input) => {
-    if (input.checked) {
-      searchTypeValue = searchTypeValue.concat(input.name + ',');
-    }
-  });
-  searchType.value = searchTypeValue;
-  searchBarForm.submit();
-});
+}
