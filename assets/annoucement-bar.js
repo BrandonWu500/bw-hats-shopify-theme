2025a9c672e1e0eac58e8ad68e2cd81eb40fe439
BrandonWu500 <brwu17@gmail.com>
2023-12-11 09:20:14 -0500

    feat: create language selector

--- assets/annoucement-bar.js
+++ assets/annoucement-bar.js
@@ -4,10 +4,22 @@ const countryCodeValueInput = document.querySelector(
 const countryCodeOptions = document.querySelectorAll(
   '.localization-country-code-option'
 );
+const languageCodeValueInput = document.querySelector(
+  '.localization-language-code-value'
+);
+const languageCodeOptions = document.querySelectorAll(
+  '.localization-language-code-option'
+);
 const localizationForm = document.querySelector('#localization_form');
 countryCodeOptions.forEach((option) => {
   option.addEventListener('click', () => {
-    countryCodeValueInput.value = option.dataset.isoCode;
+    countryCodeValueInput.value = option.dataset.countryIsoCode;
+    localizationForm.submit();
+  });
+});
+languageCodeOptions.forEach((option) => {
+  option.addEventListener('click', () => {
+    languageCodeValueInput.value = option.dataset.languageIsoCode;
     localizationForm.submit();
   });
 });
--- assets/app.js
+++ assets/app.js
@@ -3,3 +3,5 @@ document.querySelector('#mobile-menu').classList.remove('hidden');
 document.querySelector('#mobile-filters')?.classList.remove('hidden');
 document.querySelector('.country-selector-values')?.classList.remove('hidden');
 document.querySelector('.country-selector-values')?.classList.add('flex');
+document.querySelector('.language-selector-values')?.classList.remove('hidden');
+document.querySelector('.language-selector-values')?.classList.add('flex');
